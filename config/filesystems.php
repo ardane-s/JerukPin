5677d4f7b38056e846426652fdb937379957f5d4
ardane-s <komangaris2910@gmail.com>
2025-11-23 17:45:34 +0800

    Fix 500 Error: Add explicit credentials to cloudinary disk config

--- config/filesystems.php
+++ config/filesystems.php
@@ -61,6 +61,9 @@ return [
 
         'cloudinary' => [
             'driver' => 'cloudinary',
+            'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+            'api_key'    => env('CLOUDINARY_API_KEY'),
+            'api_secret' => env('CLOUDINARY_API_SECRET'),
             'notification_url' => env('CLOUDINARY_NOTIFICATION_URL'),
         ],
 
